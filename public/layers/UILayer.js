e4da2a8495177e90344111c892233cb65714a9ee
Oli <oliverholden11@gmail.com>
2026-02-23 17:11:55 +0000

    UI layer fixes

--- public/layers/UILayer.js
+++ public/layers/UILayer.js
@@ -1,31 +1,3 @@
-
-
-var MainForm = L.Control.extend({
-  options: {
-    position: "topright",
-  },
-
-  onAdd: function (map) {
-    var container = L.DomUtil.create("div", "leaflet-bar leaflet-control custom-form");
-
-    L.DomEvent.disableClickPropagation(container);
-
-    container.innerHTML = `
-      <form id="UIForm">
-        <h2 style="text-align:center">Find a FEP</h2>
-        <input id="searchFunc" type="text" oninput="runSearch(this);" placeholder="Search here..." />
-        <br>
-        <table id="resultsTable">
-        </table>
-        <br>
-        <div id="paginationControls" style="margin-top: 10px; text-align: center;"></div>
-      </form>
-    `;
-
-    return container;
-  },
-});
-
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('UIForm');
     if (form) {
@@ -53,4 +25,22 @@ var Key = L.Control.extend({
 
     return container;
   },
+});
+
+var MapGoBack = L.Control.extend({
+	options: {
+		position: "topleft",
+	},
+
+	onAdd: function (map) {
+		var container = L.DomUtil.create("div", "leaflet-bar leaflet-control custom-form");
+		L.DomEvent.disableClickPropagation(container);
+
+		container.innerHTML = `
+		<form id="keyForm">
+		<!-- Empty for now -->
+		</form>`;
+
+		return container;
+	},
 });
